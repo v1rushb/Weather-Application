a3aa81dd474efd44bf9dc2364d5692e4134a4fe9
v1rushb <cs.bashar.herbawi@gmail.com>
2024-09-21 08:44:23 +0300

    Add Observer & BotManger & Bots

---
+++ Services/Rainbot.cs
@@ -0,0 +1,34 @@
+using WeatherApp.Interfaces;
+using WeatherApp.Models;
+using WeatherApp.Services.Interfaces;
+
+namespace WeatherApp.Services.Bots
+{
+    public class RainBot : IWeatherBot, IWeatherDataObserver
+    {
+        public bool IsEnabled { get; set; }
+        private double HumidityThreshold { get; }
+        private string? Message { get; }
+
+        public RainBot(BotConfigDTO config)
+        {
+            IsEnabled = config.IsEnabled;
+            HumidityThreshold = config.Threshold;
+            Message = config.Message;
+        }
+
+        public void Update(WeatherStateDTO data)
+        {
+            if (data.Humidity >= HumidityThreshold)
+            {
+                ActivateBot();
+            }
+        }
+
+        private void ActivateBot()
+        {
+            System.Console.WriteLine("RainBot activated!");
+            System.Console.WriteLine($"RainBot: \"{Message}\"");
+        }
+    }
+}
