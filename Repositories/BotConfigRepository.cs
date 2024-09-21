e281a2232e16f6f1294ef2ccadf73f1f579a45d2
v1rushb <cs.bashar.herbawi@gmail.com>
2024-09-21 08:43:40 +0300

    Bot conifg

--- Repositories/BotConfigRepository.cs
+++ Repositories/BotConfigRepository.cs
@@ -1,3 +1,4 @@
+using System.Timers;
 using Newtonsoft.Json;
 using WeatherApp.Models;
 
@@ -5,7 +6,7 @@ namespace WeatherApp.Repositories {
     public class BotConfigRepository {
         private static readonly BotConfigRepository _instance = new BotConfigRepository();
         private const string _configFilePath = @"Config/BotsConfig.json";
-        public static BotConfigRepository Intance => _instance;
+        public static BotConfigRepository Instance => _instance;
 
         private Dictionary<string, BotConfigDTO> _bots;
 
