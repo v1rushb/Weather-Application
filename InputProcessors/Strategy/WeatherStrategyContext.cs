0967cce6a57c3b63fa293a2b25b03ea14ae6eb4d
v1rushb <cs.bashar.herbawi@gmail.com>
2024-09-21 06:59:45 +0300

    Add CanParse in Input Processors

--- InputProcessors/Strategy/WeatherStrategyContext.cs
+++ InputProcessors/Strategy/WeatherStrategyContext.cs
@@ -3,15 +3,24 @@ using WeatherApp.Models;
 
 namespace WeatherApp.Strategies 
 {
-    public abstract class WeatherStrategyContext(IInputParser inputProcessStrategy)
+    public class WeatherStrategyContext
     {
-        private IInputParser _inputProcessStrategy = inputProcessStrategy;
+        private IInputParser? _inputProcessStrategy;
+
+        public WeatherStrategyContext() {}
+
+        public WeatherStrategyContext(IInputParser inputProcessStrategy) 
+        {
+            _inputProcessStrategy = inputProcessStrategy;
+        }
 
         public void SetProcessingStrategy(IInputParser strategy) 
         {
             _inputProcessStrategy = strategy;
         }
 
-        public WeatherStateDTO? WeatherDeserialize(string input) => _inputProcessStrategy.Parse(input);
+        public WeatherStateDTO? WeatherDeserialize(string input) => _inputProcessStrategy?.Parse(input);
+        public bool CanParse(string input) => _inputProcessStrategy?.CanParse(input) ?? false;
+
     } 
 }
